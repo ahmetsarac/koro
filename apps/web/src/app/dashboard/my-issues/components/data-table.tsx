3423984414458aae014b9f90395272127b96aa95
Ahmet Saraç <ahmet.src.99@gmail.com>
2026-03-08 19:33:27 +0300

    Data table overflow was fixed.

--- apps/web/src/app/dashboard/my-issues/components/data-table.tsx
+++ apps/web/src/app/dashboard/my-issues/components/data-table.tsx
@@ -74,7 +74,7 @@ export function DataTable<TData, TValue>({
   return (
     <div className="flex flex-1 min-h-0 flex-col gap-4">
       <DataTableToolbar table={table} />
-      <div className="min-h-0 flex-1 overflow-auto rounded-md border">
+      <div className="relative min-h-0 flex-1 overflow-auto rounded-md border">
         <table className="w-full caption-bottom text-xs">
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
